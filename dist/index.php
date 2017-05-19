827cf6f26cafa8c81c30c38dbc1440d1ccdf8e37
Christian Medrano <christianmikhael@yahoo.com>
2017-05-18 20:54:35 -0700

    Initial Commit

---
+++ dist/index.php
@@ -0,0 +1,193 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Surprise | An Art 405 Final Challenge</title>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<style>
+		.doge {
+			overflow: hidden;
+			max-height: 300px;
+		}
+		.doge img {
+			width: 100%;
+		}
+
+		footer {
+			padding-top: 100px;
+			padding-bottom: 100px;
+		}
+	</style>
+</head>
+<body>
+	<header>
+		<nav class="navbar navbar-inverse navbar-toggleable-md navbar-light bg-inverse">
+		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+		    <span class="navbar-toggler-icon"></span>
+		  </button>
+		  <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+		    <a class="navbar-brand" href="#">Surprise.io</a>
+		    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+		      <li class="nav-item active">
+		        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">Products</a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">About</a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">Contact</a>
+		      </li>
+		    </ul>
+		    <form class="form-inline my-2 my-lg-0">
+		      <input class="form-control mr-sm-2" type="text" placeholder="Search">
+		      <button class="btn btn-primary my-2 my-sm-0" type="submit">Search</button>
+		    </form>
+		  </div>
+		</nav>
+	</header>
+
+	<main>
+		<div class="container-fluid bg-primary text-white">
+			<div class="container py-4">
+				<div class="row">
+					<div class="col col-md-12 col-sm-12 col-lg-12">
+						<h1>Surprise!</h1>
+						<h2>There is, in fact, one more chance to earn points</h2>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="container-fluid" style="padding: 0; width: 100vw;">
+			<img src="images/clouds.jpg" alt="" style="max-width: 100vw;">
+		</div>
+		<div class="container-fluid bg-inverse text-white py-4">
+			<div class="container">
+				<div class="row">
+					<div class="col col-md-12 col-sm-12 col-lg-12">
+						<h1>The question is, can you do it?</h1>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="container py-4">
+			<div class="row">
+				<div class="col-6">
+					<h4>Pros</h4>
+					<div class="card">
+					  <ul class="list-group list-group-flush">
+					    <li class="list-group-item">
+					    	<h3>Easy Points</h3>
+					    	<br>
+					    	<p>Cras justo odio</p>
+					    </li>
+					    <li class="list-group-item">
+					    	<h3>Easy Code</h3>
+					    	<br>
+					    	<p>Cras justo odio</p>
+					    </li>
+					    <li class="list-group-item">
+					    	<h3>Easy Times</h3>
+					    	<br>
+					    	<p>Cras justo odio</p>
+					    </li>
+					  </ul>
+					</div>
+				</div>
+				<div class="col-6">
+					<h4>Cons</h4>
+					<div class="card">
+					  <ul class="list-group list-group-flush">
+					    <li class="list-group-item">
+					    	<h3>Don't Need It</h3>
+					    	<br>
+					    	<p>Cras justo odio</p>
+					    </li>
+					    <li class="list-group-item">
+					    	<h3>Don't Care</h3>
+					    	<br>
+					    	<p>Cras justo odio</p>
+					    </li>
+					    <li class="list-group-item">
+					    	<h3>Don't Know It</h3>
+					    	<br>
+					    	<p>Cras justo odio</p>
+					    </li>
+					  </ul>
+					</div>
+				</div>
+			</div>
+			
+		</div>
+		<div class="container-fluid bg-faded py-4">
+				<div class="container py-4">
+					<div class="row">
+						<div class="col-12 text-center">
+							<h3>Dogs</h3>
+						</div>
+					</div>
+
+					<div class="row">
+						<div class="col-4 doge">
+							<img src="images/dog1.jpg" alt="">
+						</div>
+						<div class="col-4 doge">
+							<img src="images/dog2.jpg" alt="">
+						</div>
+						<div class="col-4 doge">
+							<img src="images/dog3.jpg" alt="">
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-4 doge">
+							<img src="images/dog4.jpg" alt="">
+						</div>
+						<div class="col-4 doge">
+							<img src="images/dog5.jpg" alt="">
+						</div>
+						<div class="col-4 doge">
+							<img src="images/dog6.jpg" alt="">
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-4 doge">
+							<img src="images/dog7.jpg" alt="">
+						</div>
+						<div class="col-4 doge">
+							<img src="images/dog8.jpg" alt="">
+						</div>
+						<div class="col-4 doge">
+							<img src="images/dog9.jpg" alt="">
+						</div>
+					</div>
+				</div>
+
+				<div class="container text-center">
+					<p>Doge</p>
+				</div>
+		</div>
+
+		<div class="container-fluid text-white bg-inverse text-center py-4">
+			<div class="col-12">
+				<h1>Movie Time</h1>
+			</div>
+			<div class="col-12">
+				<iframe width="1280" height="720" src="https://www.youtube.com/embed/uE-1RPDqJAY" frameborder="0" allowfullscreen></iframe>
+			</div>
+		</div>
+
+
+	</main>
+	<footer class="bg-primary text-white text-center">
+		<p>The End <br> &copy;2017</p>
+
+		<p>
+		(•_•)    ( •_•)>⌐■-■    (⌐■_■)
+		</p>
+
+	</footer>
+
+</body>
+</html>
---
+++ docs/dist/index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Surprise | An Art 405 Final Challenge</title>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+</head>
+<body>
+	<header>
+		<nav class="navbar navbar-inverse navbar-toggleable-md navbar-light bg-inverse">
+		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+		    <span class="navbar-toggler-icon"></span>
+		  </button>
+		  <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+		    <a class="navbar-brand" href="#">Surprise.io</a>
+		    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+		      <li class="nav-item active">
+		        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">Products</a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">About</a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">Contact</a>
+		      </li>
+		    </ul>
+		    <form class="form-inline my-2 my-lg-0">
+		      <input class="form-control mr-sm-2" type="text" placeholder="Search">
+		      <button class="btn btn-primary my-2 my-sm-0" type="submit">Search</button>
+		    </form>
+		  </div>
+		</nav>
+	</header>
+
+	<main>
+		<div class="container-fluid bg-primary text-white">
+			<div class="container">
+				<div class="row">
+					<div class="col col-md-12 col-sm-12 col-lg-12">
+						<h1>Surprise!</h1>
+						<h2>There is, in fact, one more chance to earn points</h2>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="container-fluid" style="padding: 0; width: 100vw;">
+			<img src="images/clouds.jpg" alt="" style="max-width: 100vw;">
+		</div>
+		<div class="container-fluid bg-inverse text-white">
+			<div class="container">
+				<div class="row">
+					<div class="col col-md-12 col-sm-12 col-lg-12">
+						<h1>The question is, can you do it?</h1>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="container">
+			<div class="row">
+				<div class="col-6">
+					<h4>Pros</h4>
+					<div class="card">
+						
+					</div>
+				</div>
+				<div class="col-6">
+					<h4>Cons</h4>
+					<div class="card">
+						
+					</div>
+				</div>
+			</div>
+		</div>
+	</main>
+
+</body>
+</html>
